8c02c55a167a6262004008d8fd256fc67e6a40e3
Thomas <suncharn.pipithkul@gmail.com>
2022-06-29 22:28:45 -0400

    mongoose connection logic

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+
+// on & once from NodeJs
+// Mongoose connections are "EventEmitter"s
+// on error event => console.error "connection error:"
+// once open event => console.log "Database connected"
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
