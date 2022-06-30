69aee95ac66f8ffb6a46f63d311ef96448310930
Thomas <suncharn.pipithkul@gmail.com>
2022-06-30 02:43:13 -0400

    simple add new campground page + POST route to mongodb

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const { urlencoded } = require('express');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -21,6 +22,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+app.use(express.urlencoded({ extended: true })); // how to parse req.body
+
 app.get('/', (req, res) => {
   res.render('home.ejs');
 });
@@ -28,6 +31,16 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render('campgrounds/index.ejs', { campgrounds });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new.ejs');
+});
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get('/campgrounds/:id', async (req, res) => {
