0daf30a903ad591bed79e6eff3ebbdd0467d959d
Thomas <suncharn.pipithkul@gmail.com>
2022-07-01 00:57:00 -0400

    update seed generator to include price, description, and image (url string)

--- seeds/index.js
+++ seeds/index.js
@@ -17,15 +17,19 @@ db.once('open', () => {
 });
 
 const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
-const random = (arr) => Math.floor(Math.random() * arr.length);
+const random = (max) => Math.floor(Math.random() * max);
 
 const seedDB = async (amount) => {
   await Campground.deleteMany({});
   for (let i = 0; i < amount; i++) {
-    const rand = random(cities);
+    const rand = random(cities.length);
+    const price = random(20) + 10;
     const camp = new Campground({
       location: `${cities[rand].city}, ${cities[rand].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description: '    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, molestias consectetur aliquid praesentium beatae maxime labore fuga, fugit hic optio possimus eaque mollitia fugiat? Eveniet cumque laudantium facere vel voluptas?',
+      price
     });
 
     await camp.save();
