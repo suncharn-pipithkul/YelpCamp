1ec8b17dab58eeaf128b24751cc193257f51387a
Thomas <suncharn.pipithkul@gmail.com>
2022-07-03 01:10:35 -0400

    simple catch all 404 error route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -44,6 +45,8 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.post('/campgrounds', catchAsync(async (req, res) => {
+  if (!req.body.campground)
+    throw new ExpressError('Invalid Campground Data', 400);
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
@@ -73,8 +76,13 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   res.redirect(`/campgrounds`);
 }));
 
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page Not Found', 404));
+});
+
 app.use((err, req, res, next) => {
-  res.send('Oh boy, something went wrong!');
+  const { statusCode = 500, message = 'Something went wrong' } = err;
+  res.status(statusCode).send(message);
 });
 
 
