bcd5b916fd650a3da9317c230a0c8b84f3f71721
Thomas <suncharn.pipithkul@gmail.com>
2022-07-03 01:34:02 -0400

    simple error template w/ ejs

--- app.js
+++ app.js
@@ -81,8 +81,10 @@ app.all('*', (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = 'Something went wrong' } = err;
-  res.status(statusCode).send(message);
+  const { statusCode = 500 } = err;
+  if (!err.message)
+    err.message = 'Oh no, something went wrong!';
+  res.status(statusCode).render('error.ejs', { err });
 });
 
 
