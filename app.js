e0f2b55ce257d6d9dcad57093a53fcae7fb8683c
Thomas <suncharn.pipithkul@gmail.com>
2022-07-05 22:19:09 -0400

    route to delete a review in a campground from MongoDB

--- app.js
+++ app.js
@@ -118,6 +118,17 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)
   res.redirect(`/campgrounds/${campground._id}`);
 }));
 
+// Delete campground review route
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+  const { id, reviewId} = req.params;
+
+  // pull all instance of a value(s) from an existing array that match a specified condition
+  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId }});
+  await Review.findByIdAndDelete(reviewId);
+
+  res.redirect(`/campgrounds/${id}`);
+}));
+
 // Catch all 404 route
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404));
