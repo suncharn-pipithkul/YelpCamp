c99c2f37eb3261e6fe3900e7d1623aaf16653e51
Thomas <suncharn.pipithkul@gmail.com>
2022-07-08 00:18:17 -0400

    error flash when accessing/editing nonexisting campground detail page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -41,6 +41,10 @@ router.post('/', validateCampground, catchAsync(async (req, res) => {
 router.get('/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findById(id).populate('reviews');
+  if (!campground) {
+    req.flash('error', 'Cannot find that campground!');
+    return res.redirect('/campgrounds');
+  }
   res.render('campgrounds/show.ejs', { campground });
 }));
 
@@ -48,6 +52,10 @@ router.get('/:id', catchAsync(async (req, res) => {
 router.get('/:id/edit', catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
+  if (!campground) {
+    req.flash('error', 'Cannot find that campground!');
+    return res.redirect('/campgrounds');
+  }
   res.render('campgrounds/edit.ejs', { campground });
 }));
 
