0de429280fa45b09dfcc39d2bea73c2773dbf2f8
Thomas <suncharn.pipithkul@gmail.com>
2022-07-08 00:03:17 -0400

    simple success flash when creating a new campground

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
 const session = require('express-session');
+const flash = require('connect-flash');
 
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
@@ -43,6 +44,12 @@ const sessionConfig = {
   }
 };
 app.use(session(sessionConfig));
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 
 // Home page
