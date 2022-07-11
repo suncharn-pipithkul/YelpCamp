2f52565a97780cbe22d6478164dff0fae413fa2f
Thomas <suncharn.pipithkul@gmail.com>
2022-07-11 03:41:54 -0400

    refactor review route logic into controller file

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,39 +1,17 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true }); // pass params from main path to here
+const reviewController = require('../controllers/reviews');
 
-const Campground = require('../models/campground');
-const Review = require('../models/review');
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 
 const catchAsync = require('../utils/catchAsync');
 
 
 // Submit campground review route
-router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id);
-  const review = new Review(req.body.review);
-  review.author = req.user._id;
-  campground.reviews.push(review);
-  
-  await review.save();
-  await campground.save();
-
-  req.flash('success', 'Created new review!');
-  res.redirect(`/campgrounds/${campground._id}`);
-}));
+router.post('/', isLoggedIn, validateReview, catchAsync(reviewController.createReview));
 
 // Delete campground review route
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
-  const { id, reviewId} = req.params;
-
-  // pull all instance of a value(s) from an existing array that match a specified condition
-  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId }});
-  await Review.findByIdAndDelete(reviewId);
-
-  req.flash('success', 'Successfully deleted the review!');
-  res.redirect(`/campgrounds/${id}`);
-}));
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviewController.deleteReview));
 
 
 module.exports = router;
