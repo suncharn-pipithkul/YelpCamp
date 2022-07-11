2365069a611419244b2df471dffbcec9c3c3506f
Thomas <suncharn.pipithkul@gmail.com>
2022-07-11 03:36:49 -0400

    remove unused campground model require statement in campground route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const campgroundController = require('../controllers/campgrounds');
 
 const catchAsync = require('../utils/catchAsync');
-const Campground = require('../models/campground');
 const { isLoggedIn, validateCampground, isCampgroundAuthor } = require('../middleware');
 
 
