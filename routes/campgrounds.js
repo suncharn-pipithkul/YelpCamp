1e8e809bd4352bf42205aad01943359ba43b408f
Thomas <suncharn.pipithkul@gmail.com>
2022-07-11 22:36:00 -0400

    change new campground form submission type to multipart/form-data instead of default urlencoded so user can submit image files

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const campgroundController = require('../controllers/campgrounds');
+const multer = require('multer'); // multiform/form-data parser for form submitting file
+const upload = multer({ dest: 'uploads/' });
 
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, validateCampground, isCampgroundAuthor } = require('../middleware');
@@ -10,7 +12,11 @@ const { isLoggedIn, validateCampground, isCampgroundAuthor } = require('../middl
 // POST: Submit new campground route
 router.route('/')
   .get(catchAsync(campgroundController.index))
-  .post(isLoggedIn, validateCampground, catchAsync(campgroundController.createCampground));
+  // .post(isLoggedIn, validateCampground, catchAsync(campgroundController.createCampground));
+  .post(upload.array('image'), (req, res) => {
+    console.dir(req.files);
+    res.send(req.body);
+  })
 
 // Create new campground page
 router.get('/new', isLoggedIn, campgroundController.renderNewForm);
