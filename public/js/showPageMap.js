7bd824beefc907b88a8303584d13b5c6b4051218
Thomas <suncharn.pipithkul@gmail.com>
2022-07-15 03:34:25 -0400

    center map + marker to each campground location

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -2,10 +2,15 @@ mapboxgl.accessToken = mapboxToken;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9, // starting zoom
+  center: campground.geometry.coordinates, // starting position [lng, lat]
+  zoom: 10, // starting zoom
   projection: 'globe' // display the map as a 3D globe
 });
+
+// Create a default Marker and add it to the map.
+const marker1 = new mapboxgl.Marker()
+.setLngLat(campground.geometry.coordinates)
+.addTo(map);
 
 map.on('style.load', () => {
   map.setFog({}); // Set the default atmosphere style
