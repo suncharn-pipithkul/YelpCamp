eb149b858235df0d22b50c75d9487aa2244453a9
Thomas <suncharn.pipithkul@gmail.com>
2022-07-26 04:48:27 -0400

    add confirm dialog when user click delete a campground/review

---
+++ public/js/showConfirmDelete.js
@@ -0,0 +1,35 @@
+const deleteCampgroundForm = document.querySelector('#deleteCampgroundForm');
+const deleteReviewForms = document.querySelectorAll('.delete-review-form');
+const modalTitle = document.querySelector('#modalTitle');
+
+// Prompt to confirm Deleting a campground
+deleteCampgroundForm.title = 'Delete Campground';
+deleteCampgroundForm.addEventListener('submit', e => {
+  e.preventDefault(); // prevent form submitting
+
+  const modal = new bootstrap.Modal('#confirmModal');
+  modalTitle.textContent = e.currentTarget.title;
+  modal.show();
+
+  const confirmBtn = document.querySelector('#confirmBtn');
+  confirmBtn.addEventListener('click', e => {
+    deleteCampgroundForm.submit();
+  });
+});
+
+// Prompt to confirm Deleting a review
+deleteReviewForms.forEach(form => {
+  form.title = 'Delete Review';
+  form.addEventListener('submit', e => {
+    e.preventDefault(); // prevent form submitting
+
+    const modal = new bootstrap.Modal('#confirmModal');
+    modalTitle.textContent = e.currentTarget.title;
+    modal.show();
+
+    const confirmBtn = document.querySelector('#confirmBtn');
+    confirmBtn.addEventListener('click', e => {
+      form.submit();
+    });
+  });
+});
